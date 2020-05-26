fda35e6b68643df48af582c864c50e946bfe3f80
= <=>
2020-05-26 10:48:22 +0100

    Update policy links and filenames

--- partials/copyright-and-links.php
+++ partials/copyright-and-links.php
@@ -1,6 +1,6 @@
 <div class="text-center mb-5">
 	Copyright Al Elliott 2005-<?php echo date('Y'); ?>.
 	<br><small>
-		<a href="/about/privacy-policy" target="_blank">Your Privacy</a> | <a href="/about/cookie-policy" target="_blank">Cookies</a> | <a href="/about/disclaimer" target="_blank">Disclaimer</a>
+		<a href="/about/privacy" target="_blank">Your Privacy</a> | <a href="/about/cookies" target="_blank">Cookies</a> | <a href="/about/disclaimer" target="_blank">Disclaimer</a>
 	</small>
 </div>
